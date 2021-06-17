88fa3a13fa7154796fec36b05f10795fdd954d72
The Regular Dude from ALX <The Regular Dude from ALX>
2021-06-17 09:27:20 -0700

    fixed betty comment

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/* Main function showing the use of printf */
+/**
+ *  * main - main block
+ *   * Return: 0
+ *    */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-/*Main function showing different byte sizes */
+/**
+ *  * main - main block
+ *   * Return: 0
+ *    */
 int main(void)
 {
 		printf("Size of a char: %i byte(s)\n", sizeof(char));
